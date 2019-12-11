29b989bb1c62479130e28c336c4035e12e703cfc
Spencer McDonough <shmcdono@usc.edu>
2019-12-10 20:41:54 -0800

    new round based on distance

--- track_target.py
+++ track_target.py
@@ -517,9 +517,10 @@ def main():
               print("\tothernode(%d).trackid = %d" % (othernode.nodeid, othernode.trackid))
               if (othernode.trackid <= 0):
                   print("\n-->othernode(%d) has not found a target yet" % othernode.nodeid)
-                  new_round_trigger = False
+                  # new_round_trigger = False
             
-            if(new_round_trigger):
+            # if(new_round_trigger):
+            if(Distance(my_node, targets[0]) > args.track_range)  
               new_round = True
             else:
               AdvertiseUDP(my_node.nodeid, my_node.trackid, 0)
